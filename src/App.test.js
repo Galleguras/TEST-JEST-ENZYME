f46bfec4adbd6cee8c13195ae7b30918836264fd
Pablo Gallego Carmona <pablogallegocarmona@gmail.com>
2021-03-12 22:09:02 +0100

    4. Probando formularios con hooks

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App, { Todo } from './App';
+import App, { Todo, TodoForm } from './App';
+
 import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16.1';
 
@@ -25,8 +26,8 @@ describe('App', () => {
         />
       );
       wrapper.find('button').at(0).simulate('click');
-      expect(completeTodo.mock.calls).toEqual([[4]])
-      expect(removeTodo.mock.calls).toEqual([])
+      expect(completeTodo.mock.calls).toEqual([[4]]);
+      expect(removeTodo.mock.calls).toEqual([]);
     });
 
     it('ejecuta removeTodo cuando pincho X', () => {
@@ -46,8 +47,24 @@ describe('App', () => {
         />
       );
       wrapper.find('button').at(1).simulate('click');
-      expect(removeTodo.mock.calls).toEqual([[5]])
-      expect(completeTodo.mock.calls).toEqual([])
+      expect(removeTodo.mock.calls).toEqual([[5]]);
+      expect(completeTodo.mock.calls).toEqual([]);
+    });
+  });
+  describe('TodoForm', () => {
+    it('llamar a addTodo cuando el formulario tiene un valor', () => {
+      const addTodo = jest.fn();
+      const prevent = jest.fn();
+      //problema con la version de react
+      const wrapper = shallow(<TodoForm addTodo={addTodo} />);
+
+      wrapper
+        .find('input')
+        .simulate('change', { target: { value: 'mi nuevo todo!' } });
+      wrapper.find('form').simulate('submit', { preventDefault: prevent });
+
+      expect(addTodo.mock.calls).toEqual(['mi nuevo todo!']);
+      expect(prevent.mock.calls).toEqual([[]]);
     });
   });
 });
