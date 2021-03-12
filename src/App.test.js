3327c481962338444cf1568ecf6c6830bdf9cf90
Pablo Gallego Carmona <pablogallegocarmona@gmail.com>
2021-03-12 21:34:34 +0100

    SHALLOW RENDER Y SIMULANDO CLICKS

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,53 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App,{Todo} from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App, { Todo } from './App';
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16.1';
 
-describe("App", () => {
+//esta linea es muy importnte para que podamos correr los rest en nuestros componentes
+configure({ adapter: new Adapter() });
+describe('App', () => {
+  describe('Todo', () => {
+    it('ejecuta completetodo cuando pincho Complete', () => {
+      const completeTodo = jest.fn();
+      const removeTodo = jest.fn();
+      const index = 4;
+      const todo = {
+        text: 'Todo 1',
+        isCompleted: true,
+      };
+      const wrapper = shallow(
+        <Todo
+          completeTodo={completeTodo}
+          removeTodo={removeTodo}
+          index={index}
+          todo={todo}
+        />
+      );
+      wrapper.find('button').at(0).simulate('click');
+      expect(completeTodo.mock.calls).toEqual([[4]])
+      expect(removeTodo.mock.calls).toEqual([])
+    });
 
+    it('ejecuta removeTodo cuando pincho X', () => {
+      const completeTodo = jest.fn();
+      const removeTodo = jest.fn();
+      const index = 5;
+      const todo = {
+        text: 'Todo 1',
+        isCompleted: true,
+      };
+      const wrapper = shallow(
+        <Todo
+          completeTodo={completeTodo}
+          removeTodo={removeTodo}
+          index={index}
+          todo={todo}
+        />
+      );
+      wrapper.find('button').at(1).simulate('click');
+      expect(removeTodo.mock.calls).toEqual([[5]])
+      expect(completeTodo.mock.calls).toEqual([])
+    });
+  });
 });
